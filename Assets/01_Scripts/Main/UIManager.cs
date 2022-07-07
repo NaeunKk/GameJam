03db7cfa28acff75884e681da7807d73cffdbc41
NaeunKk <98926630+NaeunKk@users.noreply.github.com>
2022-07-07 19:34:36 +0900

    init setting

---
+++ Assets/01_Scripts/Main/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
